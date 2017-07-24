384e2d5bab7f2b5a4a89368d4753f83b7aa6a63c
sihhan <thehappyend95@gmail.com>
2017-07-25 00:08:37 +0800

    用户可以点 ❤ 收藏贴文，并且用 Ajax 效果改进 UI。

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,13 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, :dependent => :destroy
   has_many :liked_users, :through => :likes, :source => :user
-
+  has_many :collects, :dependent => :destroy
+  has_many :collectd_posts, :through => :collects, :source => :post
   def find_like(user)
     self.likes.where( :user_id => user.id ).first
+  end
+  def find_collect(user)
+    self.collects.where( :user_id => user.id ).first
   end
 
 end
