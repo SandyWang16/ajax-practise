c773952bc946b0e79146afb7ca13b91a57b944df
sihhan <thehappyend95@gmail.com>
2017-07-24 23:13:44 +0800

    2-4 浏览贴文和产生假资料

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "/jquery-3" => "pages#jquery_3"
   get "/jquery-4" => "pages#jquery_4"
   get "/jquery-5" => "pages#jquery_5"
-  root "pages#jquery_1"
+
+  resources :posts
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
