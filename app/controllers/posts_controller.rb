a957586b079d6c6946ce6042363a7a7d94645c21
sihhan <thehappyend95@gmail.com>
2017-07-25 22:47:04 +0800

    4-4 使用核选方块(checkbox)做开关

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,6 +46,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def toggle_flag
+    @post = Post.find(params[:id])
+
+    if @post.flag_at
+      @post.flag_at = nil
+    else
+      @post.flag_at = Time.now
+    end
+
+    @post.save!
+
+    render :json => { :message => "ok", :flag_at => @post.flag_at, :id => @post.id }
+  end
+
   def uncollect
     @post = Post.find(params[:id])
     collect = @post.find_collect(current_user)
